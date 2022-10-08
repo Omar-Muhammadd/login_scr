3a2072376df1de9ed008c7c342149f932fb02f08
Omaar <Omaar5538@gmail.com>
2022-10-08 15:53:47 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:login_scr/loginScreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: loginScreen(),
+    );
+  }
+}
+
